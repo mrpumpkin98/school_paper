0dd328c8bde3aaa30b366fe50d09e54124056696
mrpumpkin98 <sju4486@naver.com>
2023-11-29 22:36:14 +0900

    feat: GlobalStyles추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
+import { GlobalStyles } from "../styles/globalStyles";
 
 function MyApp({ Component, pageProps }: any) {
   return (
     <>
       <RecoilRoot>
+        <GlobalStyles />
         <Component {...pageProps} />
       </RecoilRoot>
     </>
